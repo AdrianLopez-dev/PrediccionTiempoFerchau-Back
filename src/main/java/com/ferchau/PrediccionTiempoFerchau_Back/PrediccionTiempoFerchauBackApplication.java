0192c6b4308000f7e47f28f708448279f177f48b
AdrianLopezDiazTendero <adrian.lopez788@comunidadunir.net>
2024-09-13 23:44:29 +0200

    Creacion Dockerfile

---
+++ src/main/java/com/ferchau/PrediccionTiempoFerchau_Back/PrediccionTiempoFerchauBackApplication.java
@@ -0,0 +1,13 @@
+package com.ferchau.PrediccionTiempoFerchau_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrediccionTiempoFerchauBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrediccionTiempoFerchauBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferchau/PrediccionTiempoFerchau_Back/PrediccionTiempoFerchauBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferchau.PrediccionTiempoFerchau_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrediccionTiempoFerchauBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
